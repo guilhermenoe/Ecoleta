2f7b90543316749f9a3b634473cc3c6e06dab9ec
Guilherme Noe <guinoesil@hotmail.com>
2020-10-07 14:58:00 -0300

    iniciando o backend com nodeJS

---
+++ src/server.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const server = express()
+
+//configurar pasta publica
+server.use(express.static("public"))
+
+//configurar caminhos ou rotas
+//página inicial
+//req: Requisição
+//res: Resposta
+server.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html")
+})
+
+server.get("/create-point", (req, res) => {
+    res.sendFile(__dirname + "/views/create-point.html")
+})
+
+// ligar o servidor
+server.listen(3000)
+
+//parei no minuto 57:00
